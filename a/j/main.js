ad7adea895ea6a3edafac015cbb88910fcd36ef3
Frank West <fjwest@gmail.com>
2014-01-27 21:00:28 +0000

    JS addon: scrollToTop functionality
    New JS script to enable scrollToTop and scrolling to any element from
    any element.

--- a/j/main.js
+++ a/j/main.js
@@ -1,10 +1,15 @@
 /*******************************************************************
 * launch                                                     v 1.0 *
+*                                                                  *
+* Launches the JS framework, will initialise all core              *
+* functionality                                                    *
+*                                                                  *
 *******************************************************************/
 
 var fWcore = fWcore || {};
 
 fWcore.launch = (function () {
+
 
   /*****************************************************************
   * init                                                     v 1.0 *
@@ -14,21 +19,21 @@ fWcore.launch = (function () {
   *****************************************************************/
 
   function init() {
-    pageSetup();
+    $('html').removeClass('noJS'); // Enable JavaScript mode
+    offScreenMenu(); // Activate off screen menu
+    fWcore.scrollToTop.init(); // Activate default scroll to top functionality
   }
 
+  
   /*****************************************************************
-  * pageSetup                                                v 1.0 *
+  * offScreenMenu                                            v 1.0 *
   *                                                                *
-  * Standard page setup code run on every page load                *
+  * Switches menu classes to toggle menu on or off screen, will    *
+  * slide in via transitions in modern browsers.                   *
   *                                                                *
   *****************************************************************/
-  
-  function pageSetup() {
-    
-    // Remove noJS class
-    $('html').removeClass('noJS');
-    
+
+  function offScreenMenu() {
     // Setup offscreen menu events
     if (fWcore.settings.offScreenMenu) {
       var bodyDOM = $('body');
@@ -55,6 +60,63 @@ fWcore.launch = (function () {
     
 })();
 
+
+
+/*******************************************************************
+* scrollToTop                                                v 1.0 *
+*                                                                  *
+* Plugin for scrolling to the top of the page, or a given element  *
+*                                                                  *
+*******************************************************************/
+
+fWcore.scrollToTop = (function () {
+
+
+  /*****************************************************************
+  * init                                                     v 1.0 *
+  *                                                                *
+  * Initialises scroll to top on standard elements                 *
+  *                                                                *
+  *****************************************************************/
+
+  function init() {
+    scrollTo($('.backToTop'), $('#top'), 1000);
+  }
+
+
+  /*****************************************************************
+  * scrollTo                                                 v 1.0 *
+  *                                                                *
+  * Scrolls to a given element upon clicking another element.      *
+  *                                                                *
+  * Options                                                        *
+  * clicked: HTML object of element to be clicked                  *
+  * target: HTML object of element to scroll to                    *
+  * time: Time to spend scrolling in MS                            *
+  *                                                                *
+  *****************************************************************/
+  
+  function scrollTo(clicked, target, time) {
+    clicked.on('click', function(e) {
+      e.preventDefault();
+      $('html, body, document').animate({
+        scrollTop: target.offset().top
+      }, time);
+    });
+  }
+  
+  return {
+    init: init,
+    scrollTo: scrollTo
+  };
+    
+})();
+
+
+
+/*******************************************************************
+* Let it begin                                                     *
+*******************************************************************/
 
 $(document).ready(function () {
   fWcore.launch.init();
