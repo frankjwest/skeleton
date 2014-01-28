5cbae5e224b62e74f5e91aed9d5a4687eaf38fdb
Frank West <fjwest@gmail.com>
2014-01-28 21:43:09 +0000

    Bye compass, hello Grunt
    Removed compass, added grunt.
    Restructured CSS/JS to allow for generated files and move working files
    into dev folders.

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    watch: {
+      scripts: {
+        files: ['a/j/dev/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: ['a/s/*.scss'],
+        tasks: ['sass:dev'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    
+    concat: {
+      all: {
+        src: [
+          'a/j/dev/*.js'
+        ],
+        dest: 'a/j/live.js',
+      }
+    },
+    
+    uglify: {
+      all: {
+        src: 'a/j/live.js',
+        dest: 'a/j/live.min.js',
+      }
+    },
+
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          lineNumbers: true
+        },
+        files: {
+          'a/c/dev/live.css': 'a/s/main.scss',
+          'a/c/dev/internal-style-guide.css': 'a/s/internal-style-guide.scss'
+        }
+      },
+      build: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'a/c/live.min.css': 'a/s/main.scss',
+          'a/c/internal-style-guide.min.css': 'a/s/internal-style-guide.scss'
+        }
+      }
+    },
+    
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          hostname: '*'
+        }
+      }
+    }
+    
+  });
+
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect', 'watch', 'concat', 'uglify', 'sass:dev', 'sass:build']);
+
+};
---
+++ a/j/live.js
@@ -0,0 +1,144 @@
+/*******************************************************************
+* launch                                                     v 1.0 *
+*                                                                  *
+* Launches the JS framework, will initialise all core              *
+* functionality                                                    *
+*                                                                  *
+*******************************************************************/
+
+var fWcore = fWcore || {};
+
+fWcore.launch = (function () {
+
+
+  /*****************************************************************
+  * init                                                     v 1.0 *
+  *                                                                *
+  * Init the main init function to load plugins                    *
+  *                                                                *
+  *****************************************************************/
+
+  function init() {
+    $('html').removeClass('noJS'); // Enable JavaScript mode
+    offScreenMenu(); // Activate off screen menu
+    fWcore.scrollToTop.init(); // Activate default scroll to top functionality
+  }
+
+  
+  /*****************************************************************
+  * offScreenMenu                                            v 1.0 *
+  *                                                                *
+  * Switches menu classes to toggle menu on or off screen, will    *
+  * slide in via transitions in modern browsers.                   *
+  *                                                                *
+  *****************************************************************/
+
+  function offScreenMenu() {
+    // Setup offscreen menu events
+    if (fWcore.settings.offScreenMenu) {
+      var bodyDOM = $('body');
+      $('.toggleMenuOn, .toggleMenuOff').on('click', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        if (bodyDOM.hasClass('showMenu')) {
+          bodyDOM.removeClass('showMenu')
+          $(document).off('click.menuVisible');
+        } else {
+          bodyDOM.addClass('showMenu')
+          $(document).on('click.menuVisible', function(e) {
+            e.preventDefault();
+            $('.toggleMenuOn').trigger('click');
+          });
+        }
+      });
+    }
+  }
+  
+  return {
+    init: init
+  };
+    
+})();
+
+
+
+/*******************************************************************
+* scrollToTop                                                v 1.0 *
+*                                                                  *
+* Plugin for scrolling to the top of the page, or a given element  *
+*                                                                  *
+*******************************************************************/
+
+fWcore.scrollToTop = (function () {
+
+
+  /*****************************************************************
+  * init                                                     v 1.0 *
+  *                                                                *
+  * Initialises scroll to top on standard elements                 *
+  *                                                                *
+  *****************************************************************/
+
+  function init() {
+    scrollTo($('.backToTop'), $('#top'), 1000);
+  }
+
+
+  /*****************************************************************
+  * scrollTo                                                 v 1.0 *
+  *                                                                *
+  * Scrolls to a given element upon clicking another element.      *
+  *                                                                *
+  * Options                                                        *
+  * clicked: HTML object of element to be clicked                  *
+  * target: HTML object of element to scroll to                    *
+  * time: Time to spend scrolling in MS                            *
+  *                                                                *
+  *****************************************************************/
+  
+  function scrollTo(clicked, target, time) {
+    clicked.on('click', function(e) {
+      e.preventDefault();
+      $('html, body, document').animate({
+        scrollTop: target.offset().top
+      }, time);
+    });
+  }
+  
+  return {
+    init: init,
+    scrollTo: scrollTo
+  };
+    
+})();
+
+
+
+/*******************************************************************
+* Let it begin                                                     *
+*******************************************************************/
+
+$(document).ready(function () {
+  fWcore.launch.init();
+});
+/*****************************************************************
+* settings                                                 v 1.0 *
+*****************************************************************/
+
+var fWcore = fWcore || {};
+
+/*****************************************************************
+* settings                                                 v 1.0 *
+*                                                                *
+* version:        The fwCore version number                      *
+* offScreenMenu:  true | false                                   *
+*                 Is offscreen menu being used for this site?    *
+* isSupported:    Object containing supported CSS features       *
+*                                                                *
+*****************************************************************/
+
+fWcore.settings = {
+  version: 1.0,
+  offScreenMenu: true,
+  isSupported: {}
+};
